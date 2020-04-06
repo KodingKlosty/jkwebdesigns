848981951bf859f55cd4b86a751e4f9b623fdeb0
James K <jdklostermeyer@student.fullsail.edu>
2020-04-05 22:11:43 -0400

    Base Build
    installed:
    node.js 13.16
    react
    react-dom
    react-router-dom
    
    Components built:
    Header
    Footer
    
    notes:
    None

---
+++ jkwebdesigns/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/base/Header'
+import Footer from './components/base/Footer'
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
